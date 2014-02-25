12f83db600324f4e355d3b2b4ec0af77e0bbaa28
Carlos Álvarez Barba (Sharlie) <sharlie89@gmail.com>
2014-02-25 13:19:27 +0100

    Added the external script listar

---
+++ listar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+free -m |grep -i total | awk -F" " '{printf "\t"$1"\t" $2"\t" $3"\n"}' >> /root/temp.txt && free -m |grep -i mem | awk -F" " '{printf $1"\t" $2"\t" $3"\t" $4"\n"}' >>/root/temp.txt && free -m |grep -i swap $
+
+exit 0
+
