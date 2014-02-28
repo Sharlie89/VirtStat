54eaa4528d9131473cae0fb3580dc6a442c26624
Carlos Álvarez Barba (Sharlie) <sharlie89@gmail.com>
2014-02-28 11:32:13 +0100

    Added listar.sh

--- listar.sh
+++ listar.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Extra script for extract mem info from remote hosts
+
 free -m |grep -i total | awk -F" " '{printf "\t"$1"\t" $2"\t" $3"\n"}' >> /root/temp.txt && free -m |grep -i mem | awk -F" " '{printf $1"\t" $2"\t" $3"\t" $4"\n"}' >>/root/temp.txt && free -m |grep -i swap $
 
 exit 0
